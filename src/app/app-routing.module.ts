5f0105b2803f760bcc4e0c51203055f1908446e0
AlexisN3 <alexis.martin@isen.yncrea.fr>
2022-12-16 14:18:14 +0100

    'routing1'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { RegionsComponent } from './regions/regions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'languages', component: LanguagesComponent},
+  {path: 'regions', component: RegionsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
